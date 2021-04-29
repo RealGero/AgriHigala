2ae13901c84b4d25a454ccbe6bd2c43c49891ee1
Jackarly <jcbermudez3033@gmail.com>
2021-04-30 07:49:03 +0800

    Optimize footer.

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,18 +1,28 @@
-<div class="footer">
+<div class="footer text-muted mt-5">
+    <hr>
     <div class="row">
         <div class="col-md-4">
             <img src="/images/cdo_agri.jpg" alt="cdo aghrihigala logo">
-            <p>CDO City of Agriculture Office</p>
+            <p>
+                Cagayan de Oro
+                <br>
+                City Agriculture Office
+            </p>
         </div>
         
         <div class="col-md-4">
             <img src="/images/ustp.png" alt="ustp logo">
-            <p>University Of Science and Technology
-            of Southern Philippines</p>
+            <p>
+                University Of Science and 
+                <br> 
+                Technology of Southern Philippines
+            </p>
         </div>
         <div class="col-md-4">
             <img src="/images/CITC_logo.png" alt="citc logo">
-            <p>USTP-College of Information
+            <p>
+                USTP-College of Information
+                <br>
                 Technology & Computing
             </p>
         </div>

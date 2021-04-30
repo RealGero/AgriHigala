a383629eac92002ad1399dadfebb0c38c1e16c02
GeraldCabs <gero.cabarrubias@gmail.com>
2021-04-30 14:03:01 +0800

    no message

--- app/Rider.php
+++ app/Rider.php
@@ -48,17 +48,18 @@ class Rider extends Model
     }
 
 
-    public static function orderLines($id)
-    {
+    // public static function orderLines($id)
+    // {
 
-        $riderOrderLines = DB::table('orderlines as a')
-        ->join('stocks as b','b.stock_id','a.stock_id')
-        ->join('products as c','c.product_id','b.product_id')
-        ->join('product_types as d','d.product_type_id','c.product_type_id')
-        ->join('prices as e','e.stock_id','b.stock_id')
-        ->join('units as f','f.unit_id','e.unit_id')
-        ->where('a.order_id',$id)
-        ->get();  
+    //     $riderOrderLines = DB::table('orderlines as a')
+    //     ->join('stocks as b','b.stock_id','a.stock_id')
+    //     ->join('products as c','c.product_id','b.product_id')
+    //     ->join('product_types as d','d.product_type_id','c.product_type_id')
+    //     ->join('prices as e','e.stock_id','b.stock_id')
+    //     ->join('units as f','f.unit_id','e.unit_id')
+    //     ->where('a.order_id',$id)
+    //     ->get(); 
+    // } 
         
     public static function orderLines($id)
     {
